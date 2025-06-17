92fcc726281c16a582a65d45eb749922d2cc0ade
manutech2011 <emanoferreira2011@gmail.com>
2025-06-17 11:55:09 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -38,7 +38,7 @@ Dica: Utilize `head(len(df))`
 """
 
 import pandas as pd
-df = pd.read_csv('/content/empresas_dados.csv', sep=";")
+df = pd.read_csv('empresas_dados.csv', sep=";")
 st.dataframe(df.head(len(df)))
 
 """3) Calcule os indicadores Margem Líquida e ROA e salve como novas coluna da df. Depois apresente os dois indicadores no mesmo gráfico de linhas, agrupado por Ano  (peso: 1,0)
