59252847901fc840172f9cd2ea4abc069bf12a15
manutech2011 <emanoferreira2011@gmail.com>
2025-06-17 12:11:01 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -52,8 +52,8 @@ df["ROA"] = (df["Lucro Líquido"] / df["Ativo Total"]) * 100
 
 df_grouped = df.groupby("Ano")[["Margem Líquida", "ROA"]].mean().reset_index()
 st.dataframe(df)
-
-
+impot matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(10,6))
  
 ax.plot(df_grouped["Ano"], df_grouped["Margem Líquida"], marker="o", label="Margem Líquida (%)")
 ax.plot(df_grouped["Ano"], df_grouped["ROA"], marker="s", label="ROA (%)")
