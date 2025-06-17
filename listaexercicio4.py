0d9c2092f017144494c6869eb423dbd661d939d2
manutech2011 <emanoferreira2011@gmail.com>
2025-06-17 12:00:27 -0300

    Update listaexercicio4.py

--- listaexercicio4.py
+++ listaexercicio4.py
@@ -22,8 +22,7 @@ Este projeto tem como objetivo integrar análise de dados contábeis de empresas
 import streamlit as st
 
 st.set_page_config(
-    page_title="Lista de Exercícios 4",
-    page_icon"📊",
+    page_title="Lista de Exercícios 4"    
 )
 
 st.header("Projeto Final – Análise Contábil com Ajuste Econômico")
